e81b5b7481d280c2a3580e0e1e846105516b947e
Justin Miller <jayman1294@gmail.com>
2019-10-20 08:37:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     string = programmer_hash[:dennis_ritchie][:languages[0]]
+     string = programmer_hash[:dennis_ritchie][:languages]
      string
 end
 
